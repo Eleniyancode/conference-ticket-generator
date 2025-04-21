da3423da3a1cd2fa9309c1cacc8c34510bc37811
abdulrahaman ridwan <abdulrahamanr657@gmail.com>
2025-04-21 10:36:05 -0700

    update the logic to check whether the right image was upload

--- script.js
+++ script.js
@@ -9,65 +9,9 @@ const applicantGithubName = document.getElementById("applicant-githubname");
 const applicantEmail = document.getElementById("applicant-email");
 const applicantFullName = document.getElementById("applicant-fullName");
 const errorImageUpload = document.getElementById("errorImageUpload");
+const ticketImage = document.getElementById("ticketImage");
 
-//email validation
-function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  // console.log(regex.test(email));
-  return regex.test(email);
-}
-
-// console.log(validateEmail("abdulrahamanr657@gmail.com"));
-
-myForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const emailErrorMessage = document.getElementById("email-error-message");
-  const fullNameErrorMessage = document.getElementById(
-    "full-name-error-message"
-  );
-  const githubErrorMessage = document.getElementById("username-error-message");
-
-  if (fullNameInput.value === "") {
-    fullNameErrorMessage.textContent = "All fields are required";
-    fullNameErrorMessage.style.color = "red";
-  } else if (emailInput.value === "") {
-    emailErrorMessage.textContent = "All fields are required";
-    emailErrorMessage.style.color = "red";
-  } else if (githubUserName.value === "") {
-    githubErrorMessage.textContent = "All fields are required";
-    githubErrorMessage.style.color = "red";
-  } else if (!validateEmail(emailInput.value)) {
-    emailInput.style.borderBlockColor = "red";
-    emailErrorMessage.textContent = "Please enter a valid email address";
-    emailErrorMessage.style.color = "red";
-  } else {
-    emailErrorMessage.textContent = " ";
-    //hide the form on submit
-    wrapper.style.display = "none";
-
-    //show the success message
-    successMessage.style.display = "block";
-  }
-
-  //display the fullname input value on the succeeses message
-  applicantFullName.innerHTML = fullNameInput.value;
-  applicantEmail.innerHTML = emailInput.value;
-  applicantGithubName.innerHTML = githubUserName.value;
-  applicantName.innerHTML = fullNameInput.value;
-  const applicantInfo = {
-    fullName: fullNameInput.value,
-    email: emailInput.value,
-    github: githubUserName.value,
-  };
-
-  console.log(applicantInfo);
-
-  console.log(fullNameInput.value);
-  console.log(emailInput.value);
-  console.log(githubUserName.value);
-});
-
+//handling the drag, drop and click section upload image
 const dropArea = document.getElementById("dropArea");
 const imageInput = document.getElementById("imageInput");
 const preview = document.getElementById("preview");
@@ -112,7 +56,81 @@ function handleFile(file) {
     const reader = new FileReader();
     reader.onload = () => {
       preview.src = reader.result;
+      ticketImage.src = reader.result;
     };
     reader.readAsDataURL(file);
   }
 }
+
+//email validation
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // console.log(regex.test(email));
+  return regex.test(email);
+}
+
+// console.log(validateEmail("abdulrahamanr657@gmail.com"));
+
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailErrorMessage = document.getElementById("email-error-message");
+  const fullNameErrorMessage = document.getElementById(
+    "full-name-error-message"
+  );
+  const githubErrorMessage = document.getElementById("username-error-message");
+
+  // check for proper image upload or if image is upload at all
+  const imageInput = document.getElementById("imageInput");
+  const errorImageUpload = document.getElementById("errorImageUpload");
+  const imageCheckbox = document.getElementById("image-checkbox");
+  console.log(imageInput.files.length);
+  if (imageInput.files.length === 0) {
+    errorImageUpload.textContent = "";
+  } else {
+    errorImageUpload.textContent = "Please upload an image";
+    errorImageUpload.style.color = "red";
+    imageCheckbox.style.borderBlockColor = "red";
+    return;
+  }
+
+  //validation the form fields
+  if (fullNameInput.value === "") {
+    fullNameErrorMessage.textContent = "All fields are required";
+    fullNameErrorMessage.style.color = "red";
+  } else if (emailInput.value === "") {
+    emailErrorMessage.textContent = "All fields are required";
+    emailErrorMessage.style.color = "red";
+  } else if (githubUserName.value === "") {
+    githubErrorMessage.textContent = "All fields are required";
+    githubErrorMessage.style.color = "red";
+  } else if (!validateEmail(emailInput.value)) {
+    emailInput.style.borderBlockColor = "red";
+    emailErrorMessage.textContent = "Please enter a valid email address";
+    emailErrorMessage.style.color = "red";
+  } else {
+    emailErrorMessage.textContent = " ";
+    //hide the form on submit
+    wrapper.style.display = "none";
+
+    //show the success message
+    successMessage.style.display = "block";
+    //display the fullname input value on the succeeses message
+    applicantFullName.innerHTML = fullNameInput.value;
+    applicantEmail.innerHTML = emailInput.value;
+    applicantGithubName.innerHTML = githubUserName.value;
+    applicantName.innerHTML = fullNameInput.value;
+  }
+
+  // const applicantInfo = {
+  //   fullName: fullNameInput.value,
+  //   email: emailInput.value,
+  //   github: githubUserName.value,
+  // };
+
+  // console.log(applicantInfo);
+
+  // console.log(fullNameInput.value);
+  // console.log(emailInput.value);
+  // console.log(githubUserName.value);
+});
